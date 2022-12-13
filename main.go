836b7b44524a0222f5a6ef311bb6e4ac616d5ba1
Michael Feggans <michaelfeggans@Michaels-MacBook-Pro.local>
2022-12-13 17:47:26 +1100

    New changes made to separate getAllPlayers func from main

--- main.go
+++ main.go
@@ -6,88 +6,82 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
-	// "net/url"
 	"os"
+	"strings"
 )
 
+type Player struct {
+	ID            int         `json:"id`
+	First_name    string      `json:"first_name"`
+	Height        interface{} `json:"height_feet"`
+	Height_inches interface{} `json:"heigh_inches"`
+	Last_name     string      `json:"last_name"`
+	Position      string      `json:"position"`
+	Team          struct {
+		ID           int    `json:"id"`
+		Abbreviation string `json:"abbreviation"`
+		City         string `json:"city"`
+		Conference   string `json:"conference"`
+		Division     string `json:"division"`
+		Full_name    string `json:"full_name"`
+		Name         string `json:"name"`
+	} `json:"team`
+	Weight interface{} `json:"weight_pounds"`
+}
+
 func main() {
-	var page int
-	page = 1
-	// params := url.Values{}
-	// params.Add("page", page)
-	for i := 0; i <155; i++{
-		url := fmt.Sprint("https://www.balldontlie.io/api/v1/players?page=", page)
-		// resp, err := http.Get("https://www.balldontlie.io/api/v1/players"+params.Encode())
-		resp, err := http.Get(url)
+	if len(os.Args) == 1 {
+		fmt.Println("Please select a basketball pos: G for Guard, C for Centre of F for Forward")
+		return
+	} else {
+		input := os.Args[1]
+		allPlayers, err := GetAllPlayers()
 		if err != nil {
-		log.Fatalln(err)
+			log.Fatal(err)
+		}
+		fmt.Println(len(allPlayers))
+		for _, p := range allPlayers {
+			if p.Position == strings.ToUpper(input) {
+				fmt.Printf("%s %s (%s) %s \n", p.First_name, p.Last_name, p.Team.Abbreviation, p.Position)
+			}
+		}
+
+	}
+
+}
+
+func GetAllPlayers() ([]Player, error) {
+	var players []Player
+	for i := 1; i < 3; i++ {
+		url := fmt.Sprint("https://www.balldontlie.io/api/v1/players?page=", i)
+		resp, err := http.Get(url)
+		// TODO: Maybe don't exit on these errors, instead lets continue with api calls
+		if err != nil {
+			return nil, err
 		}
 
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-		log.Fatalln(err)
+			return nil, err
 		}
 
-		// sb := string(body)
-		// fmt.Printf("%T", sb)
-		// fmt.Print(sb)
-
-
-		type Player struct {
-			Data [] struct {
-				ID int `json:"id`
-				First_name string `json:"first_name"`
-				Height interface{} `json:"height_feet"`
-				Height_inches interface{} `json:"heigh_inches"`
-				Last_name string `json:"last_name"`
-				Position string `json:"position"`
-				Team struct {
-					ID int `json:"id"`
-					Abbreviation string `json:"abbreviation"`
-					City string `json:"city"`
-					Conference string `json:"conference"`
-					Division string `json:"division"`
-					Full_name string `json:"full_name"`
-					Name string `json:"name"`
-				} `json:"team`
-				Weight interface{} `json:"weight_pounds"`
-			} `json:"data"`
-			Meta [] struct {
-				Pages int `json:"total_pages"`
+		type Response struct {
+			Data []Player `json:"data"`
+			Meta struct {
+				Pages        int `json:"total_pages"`
 				Current_page int `json:"current_page"`
-				Next_page int `json:"next_page"`
-				Per_page int `json:"per_page"`
-				Total_count int `json:"total_count"`
+				Next_page    int `json:"next_page"`
+				Per_page     int `json:"per_page"`
+				Total_count  int `json:"total_count"`
 			} `json:"meta"`
-			}
-		
-		
-		var m Player
-		err = json.Unmarshal(body, &m)
-		// for i := 0; i < len(m.Data); i++ {
-		// 	fmt.Println(m.Data[i].Position)
-
-		// }
-		if len(os.Args) == 1 {
-			fmt.Println("Please select a basketball pos: G for Guard, C for Centre of F for Forward")
-			break
-		} else {
-			pos := os.Args[1:][0]
-		for i := 0; i<len(m.Data); i++ {
-			if m.Data[i].Position == strings.ToUpper(pos) {
-				fmt.Println(m.Data[i].First_name, m.Data[i].Last_name, m.Data[i].Position)
-				fmt.Println(m.Data[i].Team.Abbreviation)
-			}
 		}
-		// pos := os.Args[1:][0]
-		// for i := 0; i<len(m.Data); i++ {
-		// 	if m.Data[i].Position == strings.ToUpper(pos) {
-		// 		fmt.Println(m.Data[i].First_name, m.Data[i].Last_name, m.Data[i].Position)
-		// 		fmt.Println(m.Data[i].Team.Abbreviation)
-			}
-		// }
-		page += 1
 
+		var res Response
+		err = json.Unmarshal(body, &res)
+		if err != nil {
+			return nil, err
+		}
+		players = append(players, res.Data...)
 	}
-}	
+	return players, nil
+}
