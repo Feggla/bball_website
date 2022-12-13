d2e29ec12a995ed1fb016aa1cf39bf55494a6652
Michael Feggans <105261314+Feggla@users.noreply.github.com>
2022-12-13 16:00:47 +1100

    Bug fix for time. (#3)
    Definitely complicated the task more than needed, since I was using time.now in other parts of the code - simply formatted that to a neat 24-hour time
    Co-authored-by: Michael Feggans <michaelfeggans@Michaels-MBP.gateway>

--- greeting.go
+++ greeting.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"os"
+	"time"
 )
 
 func main() {
 	name := os.Args[1:][0]
 	var time_of_day string
-	current_time := time.Kitchen
+	dt := time.Now()
+	current_time := dt.Format("15:04:05")
 	now := time.Now().Hour()
 	if now >= 17 && now < 20 {
 		time_of_day = "evening"
@@ -18,6 +19,5 @@ func main() {
 	} else if now > 20 && now <= 7 {
 		time_of_day = "night time"
 	}
-
-	fmt.Print("Hey, ",name,". The time is ", current_time, " and it is ", time_of_day, ".")
+	fmt.Print("Hey, ", name, ". The time is ", current_time, " and it is ", time_of_day, ".")
 }
