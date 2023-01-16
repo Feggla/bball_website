6c203863804b750b9dcf9380c1bd86965b096846
Michael Feggans <105261314+Feggla@users.noreply.github.com>
2023-01-16 15:38:55 +1100

    Main (#6)
    * This is the commit message
    
    * Adding file for basketball position checker.
    
    * New changes made to separate getAllPlayers func from main
    
    * Fixed up my error logging and set the output as a text file called "error_log".
    
    * Accidentally made a commit for greeting.go with the changes. Real changes here:
    
    Updated error logging for main.go, set output to a text file in the directory.
    
    * Adding folders for basketball website
    
    Co-authored-by: Michael Feggans <michaelfeggans@Michaels-MBP.gateway>
    Co-authored-by: Michael Feggans <michaelfeggans@Michaels-MacBook-Pro.local>
    Co-authored-by: Michael Feggans <michaelfeggans@ip-172-17-40-171.ap-southeast-2.compute.internal>
    Co-authored-by: Michael Feggans <michaelfeggans@Michaels-MBP.lan>

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type Player struct {
+	ID            int         `json:"id`
+	First_name    string      `json:"first_name"`
+	Height        interface{} `json:"height_feet"`
+	Height_inches interface{} `json:"heigh_inches"`
+	Last_name     string      `json:"last_name"`
+	Position      string      `json:"position"`
+	Team          struct {
+		ID           int    `json:"id"`
+		Abbreviation string `json:"abbreviation"`
+		City         string `json:"city"`
+		Conference   string `json:"conference"`
+		Division     string `json:"division"`
+		Full_name    string `json:"full_name"`
+		Name         string `json:"name"`
+	} `json:"team`
+	Weight interface{} `json:"weight_pounds"`
+}
+
+func main() {
+	log_file := "error_log"
+	file, err := os.OpenFile(log_file, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0600)
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.Println("Logging to custom file")
+
+	if len(os.Args) == 1 {
+		fmt.Println("Please select a basketball pos: G for Guard, C for Centre of F for Forward")
+		return
+	} else {
+		input := os.Args[1]
+		allPlayers, err := GetAllPlayers()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println(len(allPlayers))
+		for _, p := range allPlayers {
+			if p.Position == strings.ToUpper(input) {
+				fmt.Printf("%s %s (%s) %s \n", p.First_name, p.Last_name, p.Team.Abbreviation, p.Position)
+			}
+		}
+
+	}
+
+}
+
+func GetAllPlayers() ([]Player, error) {
+	var players []Player
+	for i := 1; i < 3; i++ {
+		url := fmt.Sprint("https://www.balldontlie.io/api/v1/players?page=", i)
+		resp, err := http.Get(url)
+
+		if err != nil {
+			return nil, err
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		type Response struct {
+			Data []Player `json:"data"`
+			Meta struct {
+				Pages        int `json:"total_pages"`
+				Current_page int `json:"current_page"`
+				Next_page    int `json:"next_page"`
+				Per_page     int `json:"per_page"`
+				Total_count  int `json:"total_count"`
+			} `json:"meta"`
+		}
+
+		var res Response
+		err = json.Unmarshal(body, &res)
+		if err != nil {
+			return nil, err
+		}
+		players = append(players, res.Data...)
+	}
+	return players, nil
+}
